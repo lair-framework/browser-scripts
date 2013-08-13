bca73219d3940adbb19f56f50876e4e372bc1c6e
tomsteele <thomasjsteele@gmail.com>
2013-08-13 00:37:57 -0700

    Adds grey_hosts_no_ports_green

---
+++ grey_hosts_no_ports_green.js
@@ -0,0 +1,24 @@
+var greyHostsnoPortsGreen = function() {
+  // Loops through each host from the selected project
+  // and changes the status of any gray hosts with no open ports
+  // to green
+  // 
+  // created by: Dan Kottmann
+  var PROJECT_ID = Session.get('projectId');
+  var MODIFIED_BY = Meteor.user().emails[0].address;
+  var hosts = Hosts.find({'project_id': PROJECT_ID, 'status': 'lair-grey'}).fetch();
+  if(typeof hosts === 'undefined' || hosts.length === 0) {
+    console.log("No hosts found");
+  } else {
+    var c = 0;
+    hosts.forEach(function(host) {
+      var portCount = Ports.find({'host_id': host._id, 'port': {$gt: 0}}).count();
+      if(portCount == 0) {
+        c++;
+        console.log("Updating: " + host.string_addr);
+        Hosts.update({'_id': host._id}, {$set: {'status': 'lair-green', 'last_modified_by': MODIFIED_BY}});
+      }
+    });
+    console.log(c + " host(s) updated");
+  }
+};
