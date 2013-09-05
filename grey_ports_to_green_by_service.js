1815be6490c417cdab2fe0e0f7b27e5ca0ed7e0b
tomsteele <thomasjsteele@gmail.com>
2013-09-05 15:26:24 -0700

    Removes counter variable

--- grey_ports_to_green_by_service.js
+++ grey_ports_to_green_by_service.js
@@ -7,16 +7,14 @@ var greyPortsToGreenByService = function(service) {
   var SERVICE = service;
   var PROJECT_ID = Session.get('projectId');
   var MODIFIED_BY = Meteor.user().emails[0].address;
-  var ports= Ports.find({'project_id': PROJECT_ID, 'status': 'lair-grey', 'service': SERVICE}).fetch();
+  var ports = Ports.find({'project_id': PROJECT_ID, 'status': 'lair-grey', 'service': SERVICE}).fetch();
   if(typeof ports === 'undefined' || ports.length === 0) {
     console.log("No ports found");
   } else {
-    var c = 0;
     ports.forEach(function(port) {
-      c++;
       console.log("Updating: " + port.port + "/" + port.protocol + " (" + SERVICE + ")");
       Ports.update({'_id': port._id}, {$set: {'status': 'lair-green', 'last_modified_by': MODIFIED_BY}});
     });
-    console.log(c + " port(s) updated");
+    console.log(ports.length + " port(s) updated");
   }
 }
