33e1f040b420b393617df4040331b5cf047bc4e5
tomsteele <thomasjsteele@gmail.com>
2013-09-20 10:54:28 -0700

    Adds hostnames_to_nessus.js

---
+++ hostnames_to_nessus.js
@@ -0,0 +1,19 @@
+function hostnamesToNessus() {
+  // Generate a list of hostname[string_addr] targets suitable for input into Nessus.
+  //
+  // Created by: Tom Steele
+  // Usage: hostnamesToNessus();
+  // Requires client-side updates: false
+  var hosts = Hosts.find({project_id: Session.get('projectId')}).fetch();
+  var vhostTargets = [];
+  hosts.forEach(function(host) {
+    var ip = host.string_addr;
+    host.hostnames.forEach(function(name) {
+      var item = name + '[' + ip + ']';
+      vhostTargets.push(item);
+    });
+  });
+  vhostTargets.forEach(function(item) {
+    console.log(item);
+  });
+}
