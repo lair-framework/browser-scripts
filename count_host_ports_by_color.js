b4fc0e8f5d0579845ab2a061b67295235787f5b0
mattburch <mburch44@gmail.com>
2014-02-17 11:43:07 -0500

    Update count_host_ports_by_color.js

--- count_host_ports_by_color.js
+++ count_host_ports_by_color.js
@@ -25,6 +25,6 @@ countHostPortsByColor = function(color) {
     }
   })
   for(var host in HOSTS) {
-    console.log(host + "/" + HOSTS[host]);
+    console.log(host + " (" + HOSTS[host] + ")");
   }
 }
