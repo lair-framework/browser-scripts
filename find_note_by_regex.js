93dd11f8fa96f2721acb34b31fdb753059d26464
Joey Belans <joeybelans@gmail.com>
2014-04-03 21:47:08 -0400

    Applied all requested updates to find_note_by_regex.js

--- find_note_by_regex.js
+++ find_note_by_regex.js
@@ -1,6 +1,6 @@
-var findNoteByRegex = function(note_regex, note_type) {
+var findNoteByRegex = function(noteRegex, noteType) {
   // Performs case insensitive search of the appropriate notes (both the title and contents) for the given regex
-  // note_type can be one of the following:
+  // noteType can be one of the following:
   //    all
   //    project
   //    host
@@ -13,48 +13,61 @@ var findNoteByRegex = function(note_regex, note_type) {
 
   var PROJECT_ID = Session.get('projectId');
 
-  var noteRe = new RegExp(note_regex, "i");
-  if (note_type == 'project' || note_type == 'all') {
-	  console.log("Project Notes");
-	  var curProj = Projects.findOne({'_id': PROJECT_ID}, {notes: 1});
-	  curProj.notes.forEach(function(note) {
-		  if(noteRe.test(note.title) || noteRe.test(note.content)) {
-			  console.log("\t" + note.title);
-		  }
-	  });
+  var noteRe = new RegExp(noteRegex, "i");
+  if (noteType === 'project' || noteType === 'all') {
+    console.log("Project Notes");
+    var curProj = Projects.findOne({'_id': PROJECT_ID}, {notes: 1});
+    curProj.notes.forEach(function(note) {
+      if(noteRe.test(note.title) || noteRe.test(note.content)) {
+        console.log("\t" + note.title);
+      }
+    });
   }
-  if (note_type == 'host' || note_type == 'all') {
-	  console.log("Host Notes");
-	  Hosts.find({'project_id' : PROJECT_ID}, {notes: 1}).fetch().forEach(function(host) {
-		  host.notes.forEach(function(note) {
-			  if(noteRe.test(note.title) || noteRe.test(note.content)) {
-				  console.log("\t" + host.string_addr + " -> " + note.title);
-			  }
-		  });
-	  });
+  if (noteType === 'host' || noteType === 'all') {
+    console.log("Host Notes");
+    Hosts.find({
+      'project_id' : PROJECT_ID,
+      $or: [
+	{'notes': {$elemMatch: {'title': {$regex: noteRegex, $options: 'i'}}}},
+	{'notes': {$elemMatch: {'content': {$regex: noteRegex, $options: 'i'}}}}]}, {notes: 1}).fetch().forEach(function(host) {
+      host.notes.forEach(function(note) {
+        if(noteRe.test(note.title) || noteRe.test(note.content)) {
+          console.log("\t" + host.string_addr + " -> " + note.title);
+        }
+      });
+    });
   }
-  if (note_type == 'service' || note_type == 'all') {
-	  console.log("Service Notes");
-	  Ports.find({'project_id' : PROJECT_ID}, {notes: 1}).fetch().forEach(function(port) {
-		  port.notes.forEach(function(note) {
-			  if(noteRe.test(note.title) || noteRe.test(note.content)) {
-				  var portHost = Hosts.findOne({'project_id': PROJECT_ID, '_id': port.host_id});
-				  console.log("\t" + portHost.string_addr + " -> " + port.port.toString() + " -> " + note.title);
-			  }
-		  });
-	  });
+  if (noteType === 'service' || noteType === 'all') {
+    console.log("Service Notes");
+    Ports.find({
+      'project_id' : PROJECT_ID,
+      $or: [
+	{'notes': {$elemMatch: {'title': {$regex: noteRegex, $options: 'i'}}}},
+	{'notes': {$elemMatch: {'content': {$regex: noteRegex, $options: 'i'}}}}]}, {notes: 1}).fetch().forEach(function(port) {
+      port.notes.forEach(function(note) {
+        if(noteRe.test(note.title) || noteRe.test(note.content)) {
+          var portHost = Hosts.findOne({'project_id': PROJECT_ID, '_id': port.host_id});
+          console.log("\t" + portHost.string_addr + " -> " + port.port.toString() + " -> " + note.title);
+        }
+      });
+    });
   }
-  if (note_type == 'vulnerability' || note_type == 'all') {
-	  console.log("Vulnerability Notes");
-	  Vulnerabilities.find({'project_id' : PROJECT_ID}, {notes: 1, evidence: 1}).fetch().forEach(function(vuln) {
-		  if(noteRe.test(vuln.evidence)) {
-			  console.log("\t" + vuln.title + " -> Evidence Field");
-		  }
-		  vuln.notes.forEach(function(note){
-			  if(noteRe.test(note.title) || noteRe.test(note.content)) {
-				  console.log("\t" + vuln.title + " -> " + note.title);
-			  }
-		  });
-	  });
+  if (noteType === 'vulnerability' || noteType === 'all') {
+    console.log("Vulnerability Notes");
+    Vulnerabilities.find({
+      'project_id' : PROJECT_ID,
+      $or: [
+	{'evidence': {$regex: noteRegex, $options: 'i'}},
+	{'notes': {$elemMatch: {'title': {$regex: noteRegex, $options: 'i'}}}},
+	{'notes': {$elemMatch: {'content': {$regex: noteRegex, $options: 'i'}}}}]}, {notes: 1}).fetch().forEach(function(vuln) {
+      if(noteRe.test(vuln.evidence)) {
+        console.log("\t" + vuln.title + " -> Evidence Field");
+      }
+      vuln.notes.forEach(function(note){
+        if(noteRe.test(note.title) || noteRe.test(note.content)) {
+          console.log("\t" + vuln.title + " -> " + note.title);
+        }
+      });
+    });
   }
 };
