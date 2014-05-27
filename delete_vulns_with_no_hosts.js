cc259aee88bef1962849f8ec6e35444475a673c5
ryandorey <rtdorey@gmail.com>
2014-05-27 08:53:54 -0400

    Clean up

--- delete_vulns_with_no_hosts.js
+++ delete_vulns_with_no_hosts.js
@@ -1,11 +1,11 @@
-var deleteorphanedVulnsNoHosts = function() {
+var deleteVulnsNoHosts = function() {
   // Looks at all vulnerabilities and deletes 
   // any vulnerability that has a zero (0) host count. 
   // Useful if a host was removed from the project 
   // and left orphaned vulnerabilities behind. 
   //
   //
-  // Usage: deleteorphanedVulnsNoHosts()
+  // Usage: deleteVulnsNoHosts()
   // Created by: Ryan Dorey
   // Requires client-side updates: true
 
