2a21173a5c43b668fdc8f63f2b08dc17206b499d
ryandorey <rtdorey@gmail.com>
2014-06-03 08:41:41 -0400

    Updates

--- change_ports_to_specified_color.js
+++ change_ports_to_specified_color.js
@@ -10,19 +10,19 @@ var changePortsToSpecifiedColor = function(lairPort, lairColor) {
   var PROJECT_ID = Session.get('projectId');
   var MODIFIED_BY = Meteor.user().emails[0].address;
 
-  if(lairColor != 'lair-grey' && lairColor != 'lair-blue' && lairColor != 'lair-green' && lairColor != 'lair-orange' && lairColor != 'lair-red') {
+  if(lairColor !== 'lair-grey' && lairColor !== 'lair-blue' && lairColor !== 'lair-green' && lairColor !== 'lair-orange' && lairColor !== 'lair-red') {
     console.log("Invalid color specified");
   } else {  
   
-  var ports = Ports.find({'project_id': PROJECT_ID, 'port': lairPort}).fetch();
-  if(typeof ports === 'undefined' || ports.length === 0) {
-    console.log("No ports found");
-  } else {
-    ports.forEach(function(port) {
-      console.log("Updating: " + port.port + "/" + port.protocol);
-      Ports.update({'_id': port._id}, {$set: {'status': lairColor, 'last_modified_by': MODIFIED_BY}});
-    });
-    console.log("Total of " + ports.length + " port(s) updated");
+    var ports = Ports.find({'project_id': PROJECT_ID, 'port': lairPort}).fetch();
+    if(typeof ports === 'undefined' || ports.length === 0) {
+      console.log("No ports found");
+    } else {
+      ports.forEach(function(port) {
+        console.log("Updating: " + port.port + "/" + port.protocol);
+        Ports.update({'_id': port._id}, {$set: {'status': lairColor, 'last_modified_by': MODIFIED_BY}});
+      });
+      console.log("Total of " + ports.length + " port(s) updated");
+    }
   }
-}
 };
