04cb4808f75ad52dbcecc1f03e97932b5263555e
Alex Lauerman <alex.lauerman@gmail.com>
2014-09-18 18:21:29 -0500

    Cleaned up comments

--- delete_ports_by_port_protocol_service.js
+++ delete_ports_by_port_protocol_service.js
@@ -1,15 +1,16 @@
 var deletePorts = function(port, protocol, service) {
-	//Example Inputs
-	//port = 4172;
-	//protocol = "udp";
-	//service = "unknown";
+	//Script to delete phantom ports (mostly for UDP)
+	//Examples
+	// deletePorts(4172, "udp", "unknown");
+	//Usage
+	// deletePorts(port, protocol, service);
+	//Author: Alex Lauerman
+	//Requires client-side updates: false
 
 	var projectId = Session.get('projectId');
 	var ports = Ports.find({"project_id": projectId, "port": port, "protocol": protocol, "service": service});
 
 	ports.forEach(function(port) {
-		//console.log(port);
-		//console.log("Removing Port : " + port._id 
 		console.log("Removing Port : " + port._id +  " "+ port.port +  " " + port.protocol + " " + port.service );
 		Meteor.call('removePort', projectId, port._id, function(err) {});
 		}
@@ -17,10 +18,4 @@ var deletePorts = function(port, protocol, service) {
 
 };
 
-/*Example Usage
-port = 4172;
-protocol = "udp";
-service = "unknown";
-	
-deletePorts(port, protocol, service);
-*/
+
