c355494b5e3d272e21d14866ce397b8998f0955a
Tom Steele <thomasjsteele@gmail.com>
2015-03-31 21:09:53 -0700

    remove duplicate declaration

--- nikto_Host_List.js
+++ nikto_Host_List.js
@@ -2,8 +2,8 @@ niktoHostList = function (service, domain) {
     // Creates a list of hosts and/or hostnames for automated Nikto scan
     //
     // Created by: Matt Burch
-    // Usage: niktoHostList([/http/,80,"8000-8015"])
-    // Optional Usage: niktoHostList([/http/,80,"8000-8015"],/domain\.com/)
+    // Usage: niktoHostList([/http/,80,'8000-8015'])
+    // Optional Usage: niktoHostList([/http/,80,'8000-8015'],/domain\.com/)
     //
     if (domain && typeof domain !== 'object') {
         return console.log('Domain regex can not be a string, must be an object');
@@ -19,36 +19,37 @@ niktoHostList = function (service, domain) {
             '_id': lpid
         });
 
-        if (!(host.string_addr + ":" + port in HostTargets)) {
-            HostTargets[host.string_addr + ":" + port] = true;
+        if (!(host.string_addr + ':' + port in HostTargets)) {
+            HostTargets[host.string_addr + ':' + port] = true;
         }
         if (DOMAIN) {
             host.hostnames.forEach(function (hostname) {
-                if (DOMAIN.test(hostname) && !(hostname + ":" + port in HostTargets)) {
-                    HostTargets[hostname + ":" + port] = true;
+                if (DOMAIN.test(hostname) && !(hostname + ':' + port in HostTargets)) {
+                    HostTargets[hostname + ':' + port] = true;
                 }
             })
         }
     }
 
     SERVICES.forEach(function (service) {
+        var ports = [];
         if (typeof service == 'object') {
-            var ports = Ports.find({
+            ports = Ports.find({
                 'project_id': PROJECT_ID,
-                "service": {
-                    "$regex": service
+                'service': {
+                    '$regex': service
                 }
-            });
+            }).fetch();
             ports.forEach(function (port) {
                 getHosts(port.host_id, port.port);
             })
         } else if (typeof service == 'string') {
-            var list = service.split("-");
-            for (i = parseInt(list[0], 10); i <= parseInt(list[1], 10); i++) {
-                var ports = Ports.find({
+            var list = service.split('-');
+            for (var i = parseInt(list[0], 10); i <= parseInt(list[1], 10); i++) {
+                ports = Ports.find({
                     'project_id': PROJECT_ID,
-                    "port": i
-                });
+                    'port': i
+                }).fetch();
                 ports.forEach(function (port) {
                     getHosts(port.host_id, port.port);
                 })
@@ -56,13 +57,13 @@ niktoHostList = function (service, domain) {
         } else {
             var port = Ports.findOne({
                 'project_id': PROJECT_ID,
-                "port": service
+                'port': service
             });
             getHosts(port.host_id, port.port);
         }
-    })
+    });
 
     for (var key in HostTargets) {
         console.log(key)
     }
-}
+};
