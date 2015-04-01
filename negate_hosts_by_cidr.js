5795a8d16e03d6ef6c2f56f609d73a550031b4a4
Tom Steele <thomasjsteele@gmail.com>
2015-03-31 21:11:01 -0700

    remove unused variable and add declaration

--- negate_hosts_by_cidr.js
+++ negate_hosts_by_cidr.js
@@ -2,9 +2,8 @@ var negateHostsByCIDR = function () {
     // Generate a list of hostname[string_addr] targets which do not exist in CIDR range
     //
     // Created by: Matt Burch
-    // Usage: negateHostsByCIDR("x.x.x.x/x") or negateHostsByCIDR("x.x.x.x/x","y.y.y.y/y");
+    // Usage: negateHostsByCIDR('x.x.x.x/x') or negateHostsByCIDR('x.x.x.x/x','y.y.y.y/y');
     //
-    var hostTargets = [];
     var nets = Array.prototype.slice.call(arguments, 0);
     var hosts = Hosts.find({
         project_id: Session.get('projectId')
@@ -14,7 +13,7 @@ var negateHostsByCIDR = function () {
     function dec2Bin(octet, cidr) {
         var pad = '00000000';
         var bin = parseInt(octet[0], 10).toString(2);
-        bincidr = (bin.length >= pad.length ? bin : pad.slice(0, pad.length - bin.length) + bin);
+        var bincidr = (bin.length >= pad.length ? bin : pad.slice(0, pad.length - bin.length) + bin);
 
         for (i = 1; i <= octet.length; i++) {
             bin = parseInt(octet[i], 10).toString(2);
@@ -25,13 +24,13 @@ var negateHostsByCIDR = function () {
     }
 
     hosts.forEach(function (host) {
-        var ip = host.string_addr.split(".");
+        var ip = host.string_addr.split('.');
         hostip[dec2Bin(ip, 32)] = host.string_addr;
     });
 
     nets.forEach(function (cidr) {
-        cidr = cidr.split("/");
-        var net = cidr[0].split(".");
+        cidr = cidr.split('/');
+        var net = cidr[0].split('.');
         var netbin = dec2Bin(net, cidr[1]);
 
         for (var key in hostip) {
@@ -44,4 +43,4 @@ var negateHostsByCIDR = function () {
     for (var key in hostip) {
         console.log(hostip[key]);
     }
-}
+};
