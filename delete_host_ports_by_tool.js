892d818c257c6485e30bf038bea6d59efbd31fc0
Tom Steele <thomasjsteele@gmail.com>
2015-03-31 20:45:53 -0700

    remove unused variable and return early

--- delete_host_ports_by_tool.js
+++ delete_host_ports_by_tool.js
@@ -15,22 +15,22 @@ var deleteHostPortsByTool = function (ipAddr, lastModBy) {
         'string_addr': ipAddr
     });
     if (typeof host === 'undefined') {
-        console.log("No matching host found")
-    } else {
-
-        var ports = Ports.find({
-            'project_id': PROJECT_ID,
-            'host_id': host._id,
-            "last_modified_by": lastModBy
-        }).fetch();
-        if (typeof ports === 'undefined' || ports.length === 0) {
-            console.log("No matching ports found");
-        } else {
-            ports.forEach(function (port) {
-                console.log("Removing " + port.protocol + "/" + port.port)
-                Meteor.call('removePort', PROJECT_ID, port._id, function (err) {});
-            });
-            console.log("Total of " + ports.length + " port(s) removed.");
-        }
+        console.log("No matching host found");
+        return;
     }
+
+    var ports = Ports.find({
+        'project_id': PROJECT_ID,
+        'host_id': host._id,
+        "last_modified_by": lastModBy
+    }).fetch();
+    if (ports.length < 1) {
+        console.log("No matching ports found");
+    }
+
+    ports.forEach(function (port) {
+        console.log("Removing " + port.protocol + "/" + port.port);
+        Meteor.call('removePort', PROJECT_ID, port._id, function (err) {});
+    });
+    console.log("Total of " + ports.length + " port(s) removed.");
 };
