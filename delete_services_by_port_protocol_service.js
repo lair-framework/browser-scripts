dbb1e8004937fad5a187b46274b1e75c2cb2201a
Tom Steele <thomasjsteele@gmail.com>
2015-09-24 09:44:26 -0700

    Update delete_services_by_port_protocol_service.js

--- delete_services_by_port_protocol_service.js
+++ delete_services_by_port_protocol_service.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* globals Session Services Meteor */
 
-function deleteServices (service, protocol, service) {
+function deleteServices (port, protocol, service) {
   // Script to delete phantom services (mostly for UDP)
   // Examples:
   //  deleteServices(4172, 'udp', 'unknown')
   // Usage:
-  //  deleteServices(service, protocol, service)
+  //  deleteServices(port, protocol, service)
   //  deleteServices(0, 'udp', 'general')
   // Author: Alex Lauerman
   // Requires client-side updates: false
@@ -14,13 +14,13 @@ function deleteServices (service, protocol, service) {
   var projectId = Session.get('projectId')
   var services = Services.find({
     'projectId': projectId,
-    'service': service,
+    'port': port,
     'protocol': protocol,
     'service': service
   })
 
   services.forEach(function (service) {
-    console.log('Removing Service : ' + service._id + ' ' + service.service + '/' + service.protocol + ' ' + service.service)
-    Meteor.call('removePort', projectId, service._id, function () {})
+    console.log('Removing Service : ' + service._id + ' ' + service.port + '/' + service.protocol + ' ' + service.service)
+    Meteor.call('removeService', projectId, service._id, function () {})
   })
 }
